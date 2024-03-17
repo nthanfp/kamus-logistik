e104296d5a4374e9980e8920ab1b929d798f8d2d
Nathanael Ferry P <ntnferry1505@gmail.com>
2024-03-17 08:35:57 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\Services\OpenAIService;
 use Illuminate\Support\ServiceProvider;
+use Livewire;
+use Illuminate\Support\Facades\Route;
 
 class AppServiceProvider extends ServiceProvider
 {
